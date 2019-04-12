07966424e84064de49636b7da548af4023eb7688
Paul Kinsky <pkinsky@gmail.com>
2019-04-11 23:24:11 -0700

    cleanup, update deps

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,23 +1,11 @@
 module Main where
 
 --------------------------------------------
-import qualified Control.Concurrent               as C
-import           Control.Exception.Safe (throw, bracket)
 import           Control.Monad.Fail (MonadFail)
-import           Control.Monad.IO.Class
-import           Control.Monad.Trans.State.Lazy (StateT, gets, runStateT, modify)
+import           Control.Monad.Trans.State.Lazy (runStateT)
 import           Data.Functor.Compose
-import           Data.Map (Map)
 import qualified Data.Map as M
-import           Network.HTTP.Client (newManager, defaultManagerSettings)
-import qualified Network.Wai.Handler.Warp         as Warp
-import           Servant.Client (mkClientEnv, runClientM, Scheme(..), BaseUrl(..))
-import           System.IO.Temp
-import           System.IO
 --------------------------------------------
-import           Hedgehog
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
 import           Test.Hspec
 --------------------------------------------
 import           Data.Aeson.Orphans ()
@@ -26,9 +14,8 @@ import           HGit.Core.Merge
 import           HGit.Core.Types
 import           Merkle.Functors
 import           Merkle.Store
-import           Merkle.Store.Network
-import           Merkle.Store.FileSystem
 import           Merkle.Store.Deref
+import           Merkle.Store.Test
 import           Merkle.Types
 import           Util.RecursionSchemes
 --------------------------------------------
@@ -36,86 +23,6 @@ import           Util.RecursionSchemes
 
 main :: IO ()
 main = do
-  hSetBuffering stdout NoBuffering
-
-  let ffail :: MonadFail m => Hash x -> Fix (HashAnnotated x `Compose` m `Compose` x)
-      ffail h = Fix $ Compose (h, Compose $ fail "Boom!")
-      dir :: Applicative m
-          => [NamedFileTreeEntity (Hash Blob) (Fix (HashAnnotated HashableDir `Compose` m `Compose` HashableDir))]
-          -> Fix (HashAnnotated (Dir (Hash Blob)) `Compose` m `Compose` Dir (Hash Blob))
-      dir xs =
-        let d = Dir xs
-            h = hash $ fmap htPointer d
-         in Fix $ Compose (h, Compose $ pure d)
-      dir' n xs = (n,) . DirEntity $ dir xs
-      blobHash body = hash $ Chunk body emptyHash
-      file n = (n,) . FileEntity . blobHash
-
-  -- generate a flat structure
-  let propRoundtripShallow s =
-        property $ do
-          x <- forAll genDir
-          h <- sUploadShallow s x
-          r <- sDeref' s h
-          (fmap htPointer r) === x
-
-      genHash = do
-        seed <- Gen.string  (Range.singleton 100) Gen.alphaNum
-        pure $ doHash [unpackString seed]
-
-      genNamedFileEntity = do
-        n <- Gen.string (Range.singleton 10) Gen.alphaNum
-        (n,) <$> Gen.choice [fmap DirEntity genHash, fmap FileEntity genHash]
-
-      genDir :: Gen (HashableDir (Hash HashableDir))
-      genDir = Dir <$> Gen.list (Range.constant 1 10) genNamedFileEntity
-
-  -- generate a nested structure -> deep upload -> strict download -> check (==)
-  let propRoundtripDeep s =
-        property $ do
-          x <- forAll gen
-          h <- uploadDeep s x
-          r <- strictDeref $ lazyDeref' s h
-          stripTags r === x
-
-      gen :: MonadGen m => m (Fix HashableDir)
-      gen = anaM genDirR 5
-
-      genDirR :: MonadGen m => Int -> m (HashableDir Int)
-      genDirR n = Dir <$> Gen.list (Range.constant 1 3) (Gen.choice [genFile, genDir' n])
-
-      genDir' :: MonadGen m => Int -> m (NamedFileTreeEntity (Hash Blob) Int)
-      genDir' n = if n <= 0
-        then genFile
-        else do
-          name <- Gen.string  (Range.singleton 10) Gen.alphaNum
-          pure (name, DirEntity $ n - 1)
-
-      genFile :: MonadGen m => m (NamedFileTreeEntity (Hash Blob) Int)
-      genFile = do
-        name <- Gen.string (Range.singleton 10) Gen.alphaNum
-        body <- Gen.string (Range.singleton 100) Gen.alphaNum
-        pure $ file name body -- file w/ hash of body
-
-  -- use shared hash-addressed store for all tests - if hash == content ==, so safe. Merkle!
-  propres <- withSystemTempDirectory "proptest" $ \ fspath ->
-    withSystemTempDirectory "proptest" $ \ netpath -> do
-      let port = 8081 -- todo random?
-      manager <- newManager defaultManagerSettings
-      let env = mkClientEnv manager (BaseUrl Http "localhost" port "")
-      let netStore' :: Store (PropertyT IO) HashableDir
-          netStore' = liftStore (\mx -> liftIO (runClientM mx env) >>= either throw pure) netStore
-      bracket (liftIO . C.forkIO . Warp.run port $ app (fsStore netpath :: Store IO HashableDir))
-        C.killThread $ \_ -> do
-          checkSequential $ Group "Store.RoundTrip"
-            [ ("fs: flat Dir merkle tree round trip via store", propRoundtripShallow $ fsStore fspath)
-            , ("fs: deep Dir merkle tree round trip via store", propRoundtripDeep $ fsStore fspath)
-            , ("network: flat Dir merkle tree round trip via store", propRoundtripShallow netStore')
-            , ("network: deep Dir merkle tree round trip via store", propRoundtripDeep netStore')
-            ]
-
-  print propres
-
   hspec $ do
     describe "diff" $ do
       let diffTest r1 r2 expected = do
@@ -223,25 +130,16 @@ main = do
 
         err `shouldBe` MergeViolation ["baz", "bar"]
 
-type SSMap f = Map (Hash f) (f (Hash f))
-
--- TODO: move to Merkle.Store.Test
-testStore
-  :: forall m f
-   . ( Hashable f
-     , Functor f
-     , MonadIO m
-     )
-  => Store (StateT (SSMap f) m) f
-testStore = Store
-  { sDeref = \p -> gets (lookup' p)
-  , sUploadShallow = \x -> do
-          let p = hash x
-          modify (M.insert p x)
-          pure p
-  }
   where
-    lookup' :: Hash f -> SSMap f -> Maybe (DerefRes f)
-    lookup' p h =
-      let lr = M.lookup p h
-       in fmap (fmap (Fix . Compose . (, Compose $ Nothing))) lr
+    ffail :: MonadFail m => Hash x -> Fix (HashAnnotated x `Compose` m `Compose` x)
+    ffail h = Fix $ Compose (h, Compose $ fail "Boom!")
+    dir :: Applicative m
+        => [NamedFileTreeEntity (Hash Blob) (LazyMerkleDir m (Hash Blob))]
+        -> LazyMerkleDir m (Hash Blob)
+    dir xs =
+      let d = Dir xs
+          h = hash $ fmap htPointer d
+        in Fix $ Compose (h, Compose $ pure d)
+    dir' n xs = (n,) . DirEntity $ dir xs
+    blobHash body = hash $ Chunk body emptyHash
+    file n = (n,) . FileEntity . blobHash
